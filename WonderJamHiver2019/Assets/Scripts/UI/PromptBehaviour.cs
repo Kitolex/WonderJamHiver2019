e3e04a8a1f6a278eaef31fb5a75d957c60d1918f
Unknown <leo.posticblu@orange.fr>
2019-03-02 14:49:29 -0500

    UI lobby prompt

---
+++ WonderJamHiver2019/Assets/Scripts/UI/PromptBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PromptBehaviour : MonoBehaviour
+{
+    SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player p = other.GetComponent<Player>();
+
+        if(p && p.isLocalPlayer)
+        {
+            sr.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Player p = other.GetComponent<Player>();
+
+        if (p && p.isLocalPlayer)
+        {
+            sr.enabled = false;
+        }
+    }
+}
