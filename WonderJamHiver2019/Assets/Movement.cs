eafe4e2f5ebb59e956132b3ea9657a5175789de0
Unknown <leo.posticblu@orange.fr>
2019-03-01 19:10:25 -0500

    Création player + movement

---
+++ WonderJamHiver2019/Assets/Movement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Movement : NetworkBehaviour
+{
+    float horizontalAxis;
+    float verticalAxis;
+
+    [Range(1,10)]
+    public float movementSpeed;
+    public float acceleration;
+
+    Rigidbody rb;
+
+    bool canMove = true;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (!rb)
+            Debug.LogWarning("Pas de rigidbody sur le joueur");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        GetAxis();
+        ApplyMovement();
+    }
+
+    void GetAxis()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+    }
+
+    void ApplyMovement()
+    {
+        if (!canMove)
+            return;
+
+        Vector2 movement = new Vector2(horizontalAxis, verticalAxis);
+        movement.Normalize();
+
+        float magnitude = Mathf.Lerp(rb.velocity.magnitude, movementSpeed, Time.deltaTime * acceleration);
+
+        rb.velocity = new Vector3(movement.x * magnitude,rb.velocity.y, movement.y * magnitude);
+    }
+}
