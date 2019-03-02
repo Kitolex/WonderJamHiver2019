1d5704645ea44f7295f1398355098f725db956ce
Julie <juliemounier96@gmail.com>
2019-03-02 16:26:50 -0500

    lobby room

---
+++ WonderJamHiver2019/Assets/WindowManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowManager : MonoBehaviour
+{
+    public GameObject firstWindow ;
+    public GameObject secondWindow ;
+    public GameObject thirdWindow ;
+    public GameObject lastWindow ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstWindow.SetActive( false);
+        secondWindow.SetActive(false);
+        thirdWindow.SetActive(false);
+        lastWindow.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
