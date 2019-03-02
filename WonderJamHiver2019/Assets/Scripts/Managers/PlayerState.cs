185ec8e815569baa4bee5499e34918f90b7d7789
Fiesta87 <jb@pommeret.fr>
2019-03-02 12:51:54 -0500

    spawnPlayer

---
+++ WonderJamHiver2019/Assets/Scripts/Managers/PlayerState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public static PlayerState singleton;
+
+    public int myTeam;
+
+    public bool inGame;
+
+    void Awake()
+    {
+        if(null == PlayerState.singleton){
+            PlayerState.singleton = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTeam = 0;
+        inGame = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
