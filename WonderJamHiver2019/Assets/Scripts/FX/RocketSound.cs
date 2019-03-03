394ca2ba2e947ad341b04cce7d15ca691a1768d4
Fiesta87 <jb@pommeret.fr>
2019-03-03 14:58:59 -0500

    merge fix sound

---
+++ WonderJamHiver2019/Assets/Scripts/FX/RocketSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+public class RocketSound : MonoBehaviour,EventListener<EndGameEvent>
+{
+    public AudioClip ClipDecollage;
+
+    private AudioSource source;
+    void OnEnable()
+    {
+        this.EventStartListening<EndGameEvent>();
+    }
+    void OnDisable()
+    {
+        this.EventStopListening<EndGameEvent>();
+    }
+
+    public void OnEvent(EndGameEvent eventType)
+    {
+        StartCoroutine(EndGameCoroutine());
+    }
+
+    IEnumerator EndGameCoroutine()
+    {
+        yield return new WaitForSeconds(1);
+        source = GetComponent<AudioSource>();
+        source.PlayOneShot(ClipDecollage,1.0F);
+    }
+}
