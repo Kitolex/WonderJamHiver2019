451fda7ee99e546dea3268b24aed134a0dded10f
Unknown <brandon.piard@etu.univ-lyon1.fr>
2019-03-03 14:22:37 -0500

    Jauge OK

---
+++ WonderJamHiver2019/Assets/Scripts/UI/JaugeUpdater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JaugeUpdater : MonoBehaviour
+{
+    public Slider slider;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerState.singleton.myTeam == 1)
+        {
+            slider.value = (float)MainGameManager.singleton.teamBase1.currentPression / (float)MainGameManager.singleton.teamBase1.neededPressionToWin;
+        }
+
+        else if(PlayerState.singleton.myTeam == 2)
+        {
+            slider.value = (float)MainGameManager.singleton.teamBase2.currentPression / (float)MainGameManager.singleton.teamBase2.neededPressionToWin;
+        }
+    }
+}
